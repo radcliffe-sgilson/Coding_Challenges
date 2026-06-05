5e0b1b8fcadbe7d30b7c449621fc6417cd7f476f
Steven Gilson <249266381+radcliffe-sgilson@users.noreply.github.com>
2026-06-05 10:34:44 +0100

    Setup
    Project Setup

---
+++ Red.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TemplateProject
+{
+    public class Red
+    {
+        public static void JokesOnYou()
+        {
+            Console.WriteLine("Why do ducks have tail feathers?");
+            Console.Read();
+            Console.WriteLine("To hide their butt-quacks");
+        }
+    }
+}
