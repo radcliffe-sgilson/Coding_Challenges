5d8f7a0a738f16018da2976f2a51530c0429db4f
Steven Gilson <249266381+radcliffe-sgilson@users.noreply.github.com>
2026-06-12 09:51:42 +0100

    Update DefensiveDesign.cs

--- DefensiveDesign.cs
+++ DefensiveDesign.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -53,6 +54,113 @@ namespace TemplateProject
             }
 
             return inputValue;
+        }
+
+        public static string StringInputLength(int minChars = 1, int maxChars = 10, string msg = "")
+        {
+            bool validInput = false;
+            string returnValue = "";
+            while (!validInput)
+            {
+                Console.Write(msg);
+                string? rawInput = Console.ReadLine();
+                if (rawInput == null)
+                {
+                    Console.WriteLine("Please provide a value.");
+                }
+                else
+                {
+                    if (rawInput.Length >= minChars && rawInput.Length <= maxChars)
+                    {
+                        returnValue = rawInput;
+                        validInput = true;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Please provide a value that is at least " + minChars + " in length, but no more than " + maxChars + " in length.");
+                    }
+                }
+            }
+
+            return returnValue;
+        }
+
+        public static bool InputMaskHandler(string checkValue, string inputMask)
+        {
+            try
+            {
+                if (inputMask == null)
+                {
+                    throw new ArgumentNullException("Mask cannot be null");
+                }
+
+                return InputMaskCheck(checkValue, inputMask);
+            }
+            catch (ArgumentNullException ex)
+            {
+                Console.WriteLine($"Configuration Error: {ex.Message}");
+            }
+
+            catch (Exception ex)
+            {
+                // Catch-all for unexpected issues
+                Console.WriteLine($"Unexpected error: {ex.Message}");
+            }
+
+            return false;
+        }
+
+        public static bool InputMaskCheck(string checkValue, string inputMask)
+        {
+            //Rules:
+            // - L : Letters only
+            // - N : Digit (0-9)
+            // - A : Alphanumeric
+            // - C : Any character
+            // - space/literal : Must mach exactly
+
+            if(checkValue.Length != inputMask.Length)
+            {
+                return false;
+            }
+
+            for(int i = 0; i < inputMask.Length; i++)
+            {
+                char m = inputMask.ToUpper()[i];
+                char v = checkValue[i];
+
+                switch (m)
+                {
+                    case 'L': //Letter Required
+                        if (!char.IsLetter(v))
+                        {
+                            return false;
+                        }
+                        break;
+                    case 'N': //Digit Required
+                        if (!char.IsDigit(v))
+                        {
+                            return false;
+                        }
+                        break;
+                    case 'A': //Alphanumeric Required
+                        if (!char.IsLetterOrDigit(v))
+                        {
+                            return false;
+                        }
+                        break;
+                    case 'C':
+                        break;
+                    default: //Literal match
+                        if (v != m)
+                        {
+                            return false;
+                        }
+                        break;
+                }
+            }
+
+            return true;
         }
     }
 }
